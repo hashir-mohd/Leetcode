583de42fe0a54523e63b5cbf55f3294b93b599de
hashir-mohd <142725430+hashir-mohd@users.noreply.github.com>
2025-01-10 09:26:01 +0530

    Time: 760 ms (5.02%), Space: 251.8 MB (5.02%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        unordered_map<char,int>maxFreq;
+        for(auto b : words2){
+            for(int i =0 ;i <26;i++){
+                int count = std::count(b.begin(),b.end(),'a'+i);
+                if(maxFreq['a'+i]<count || maxFreq.find('a'+i)==maxFreq.end()){
+                    maxFreq['a'+i]=count;
+                }
+            }
+        }
+        vector<string> ans;
+        for(auto a:words1){
+            unordered_map<char,int>freq;
+            for(int i =0 ;i <26;i++){
+                int count = std::count(a.begin(),a.end(),'a'+i);
+                if(freq['a'+i]<count || freq.find('a'+i)==freq.end()){
+                    freq['a'+i]=count;
+                }
+            }
+            bool flag=true;
+            for(auto& pair:freq){
+                if(maxFreq[pair.first]>pair.second){
+                    flag=false;
+                }
+            }
+            if(flag){
+                ans.push_back(a);
+            }
+        }
+        return ans;
+    }
+};
