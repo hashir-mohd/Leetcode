9c59ca200c3036e53bdde53d925fc260cf030f58
hashir-mohd <142725430+hashir-mohd@users.noreply.github.com>
2025-03-08 13:15:54 +0530

    Time: 22 ms (88.56%), Space: 17.4 MB (48.49%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -4,25 +4,16 @@ using vvi = vector<vector<int>>;
 class Solution {
     void traversal(int row, int col, vvi& visited, vector<vector<char>>& grid) {
         visited[row][col] = 1;
-        queue<pair<int, int>> q;
-        q.push({row, col});
 
         int dx[4] = {-1, 1, 0, 0};
         int dy[4] = {0, 0, -1, 1};
 
-        while (!q.empty()) {
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-
-            for (int d = 0; d < 4; d++) {
-                int nrow = row + dx[d], ncol = col + dy[d];
-                if (nrow >= 0 && ncol >= 0 && nrow < grid.size() &&
-                    ncol < grid[0].size() && grid[nrow][ncol] == '1' &&
-                    visited[nrow][ncol] == 0) {
-                    visited[nrow][ncol] = 1;
-                    q.push({nrow, ncol});
-                }
+        for (int d = 0; d < 4; d++) {
+            int nrow = row + dx[d], ncol = col + dy[d];
+            if (nrow >= 0 && ncol >= 0 && nrow < grid.size() &&
+                ncol < grid[0].size() && grid[nrow][ncol] == '1' &&
+                visited[nrow][ncol] == 0) {
+                traversal(nrow,ncol,visited,grid);
             }
         }
     }
@@ -37,7 +28,7 @@ public:
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (visited[i][j] == 0 && grid[i][j]=='1') {
+                if (visited[i][j] == 0 && grid[i][j] == '1') {
                     islands++;
                     traversal(i, j, visited, grid);
                 }
