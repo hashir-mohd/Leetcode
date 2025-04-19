3419eb0c9ce202e7ad23de47cc9ef7bef852ae6d
hashir-mohd <142725430+hashir-mohd@users.noreply.github.com>
2025-04-19 10:47:25 +0530

    Time: 28 ms (88.23%), Space: 60.3 MB (73.14%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         sort(nums.begin(), nums.end());
-        return func(nums, upper + 1) - func(nums, lower);
+        return func(nums, lower) - func(nums, upper+1);
     }
 
 private:
     long long func(vector<int>& nums, int value) {
         int left = 0, right = nums.size() - 1;
         long long result = 0;
+        int n = nums.size();
         while (left < right) {
             int sum = nums[left] + nums[right];
-            if (sum < value) {
-                result += (right - left);
-                left++;
-            } else {
+            if (nums[left] + nums[right] >= value) {
+                result += (right - left);  // all pairs (left, left+1)...(left, right)
                 right--;
+            } else {
+                left++;
             }
         }
         return result;
