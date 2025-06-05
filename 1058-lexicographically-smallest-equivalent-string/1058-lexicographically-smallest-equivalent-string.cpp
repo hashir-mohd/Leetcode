594271a916e6ba14699e3963a54a3ad5ca1048d8
HASHIR <142725430+hashir-mohd@users.noreply.github.com>
2025-06-05 10:29:35 +0530

    Time: 1 ms (52.32%), Space: 8.8 MB (65.86%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
@@ -1,32 +1,37 @@
 class Solution {
+    vector<int> parent;
 
-    char dfs(unordered_map<char,vector<char>> &adj,char curr, vector<int> &vis){
-        vis[curr-'a']=1;
-        char minChar=curr;
-        for(char neighbour:adj[curr]){
-            if(vis[neighbour -'a']==0){
-                minChar= min(minChar,dfs(adj,neighbour,vis));
-            }
-        }
-        return minChar;
+    
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
     }
+
+    
+    void unionSet(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px == py) return;
+        if (px < py) parent[py] = px;
+        else parent[px] = py;
+    }
+
 public:
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        int n = s1.length();
-        unordered_map<char,vector<char>> adj;
-        for(int i =0;i <n; i++){
-            char u=s1[i];
-            char v=s2[i];
-            adj[v].push_back(u);
-            adj[u].push_back(v);
+        parent.resize(26);
+        for (int i = 0; i < 26; ++i)
+            parent[i] = i;
+
+        for (int i = 0; i < s1.size(); ++i) {
+            unionSet(s1[i] - 'a', s2[i] - 'a');
         }
 
-        string res;
-        for(char ch:baseStr){
-            vector<int> vis(26,0);
-            char minChar=dfs(adj,ch,vis);
-            res+=minChar;
+        string result = "";
+        for (char c : baseStr) {
+            result += (char)(find(c - 'a') + 'a');
         }
-        return res;
+
+        return result;
     }
 };
