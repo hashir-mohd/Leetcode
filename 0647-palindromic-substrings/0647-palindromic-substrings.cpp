31f6fe1c3c30bee47cbe7a63eb26cf77a6790d74
HASHIR <142725430+hashir-mohd@users.noreply.github.com>
2025-08-08 15:05:01 +0530

    Time: 39 ms (26%), Space: 34.6 MB (15.79%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    vector<vector<int>> memo;
+
+    bool isPal(int i, int j,string &s){
+        if(i>j) return true;
+        if(memo[i][j]!= -1)return memo[i][j];
+        if(s[i]==s[j]){
+            if(isPal(i+1,j-1,s)){
+                memo[i][j]=1;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+public:
+    int countSubstrings(string s) {
+        int n =s.size();
+        int count=0;
+        memo.assign(n,vector<int>(n,-1));
+        for(int i =0 ;i < n ; i++){
+            for(int j =i ;j < n; j++){
+                if(isPal(i,j,s)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
