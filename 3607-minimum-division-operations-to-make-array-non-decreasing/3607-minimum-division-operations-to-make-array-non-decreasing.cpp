3f4bf5d7a8c98ea00f80bb637aef02a0d9c3eef4
HASHIR <142725430+hashir-mohd@users.noreply.github.com>
2025-10-06 11:56:50 +0530

    Time: 1023 ms (18.72%), Space: 320.9 MB (5.91%) - LeetHub

---
+++ 3607-minimum-division-operations-to-make-array-non-decreasing/3607-minimum-division-operations-to-make-array-non-decreasing.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> computeSPF(int maxNum) {
+         vector<int> spf(maxNum + 1);
+        for (int i = 0; i <= maxNum; i++) spf[i] = i;
+        for (int i = 2; i * i <= maxNum; i++) {
+            if (spf[i] == i) { // i is prime
+                for (int j = i * i; j <= maxNum; j += i) {
+                    if (spf[j] == j) spf[j] = i;
+                }
+            }
+        }
+        return spf;
+    }
+
+    int minOperations(vector<int>& nums) {
+        if(nums.size() == 1) return 0;
+        int n = nums.size();
+        int maxNum = *max_element(nums.begin(), nums.end());
+        vector<int> spf = computeSPF(maxNum);
+
+        int count=0;
+
+
+        for(int i = nums.size()-2;i>=0;i--){
+            if(nums[i]<=nums[i+1]){
+                continue;
+            }
+            else{
+                while(nums[i]>nums[i+1]){
+                    if( spf[nums[i]]==nums[i]) {
+                        return -1;
+                    }
+                    nums[i]/=(nums[i]/spf[nums[i]]);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
