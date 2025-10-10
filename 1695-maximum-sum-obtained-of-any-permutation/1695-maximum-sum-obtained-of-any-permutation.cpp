c1d0323407e83fcf7e90b3957c73557ada6390b8
HASHIR <142725430+hashir-mohd@users.noreply.github.com>
2025-10-10 13:02:47 +0530

    Time: 37 ms (86.21%), Space: 125.1 MB (78.62%) - LeetHub

--- 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.cpp
+++ 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.cpp
@@ -18,10 +18,9 @@ public:
             }
             
         }
-        for(int i=0;i<n;i++) {
-            diff[i]+=(i>0?diff[i-1]:0);
+        for(int i=1;i<n;i++) {
+            diff[i]+=diff[i-1];
         }
-        vector<int> overlaps(n);
         sort(nums.begin(),nums.end());
         sort(diff.begin(),diff.end());
         int j=n-1;
